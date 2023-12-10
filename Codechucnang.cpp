9d90d292baed9ec0ca9892ae3e636990ab28ffb4
Trung Kien <trungkien2282005@gmail.com>
2023-12-10 23:32:58 +0700

    Codechucnang.cpp

---
+++ Codechucnang.cpp
@@ -0,0 +1,47 @@
+#include<iostream>
+using namespace std;
+/*	Dat: pt bac nhat
+	Huy: khoang cach
+	Phat: pt bac hai
+	Tien: tong 1 den n
+	Trung: Tong giai thua
+
+	THIEN KHOA CHYM NOT TO: ...LAM CAI DEO GI CHUA BIET
+*/
+int main()
+{
+	int cn;
+	do
+	{
+	
+	cout << "----------CHUONG TRINH CHUC NANG----------" << endl;
+	cout << "1. " << endl;
+	cout << "2. " << endl;
+	cout << "3. " << endl;
+	cout << "4. " << endl;
+	cout << "5. " << endl;
+	cout << "6. " << endl;
+	cout << "0. Thoat chuong trinh !!!" << endl;
+	cout << endl;
+	cout << "Chon chuc nang: ";
+	cin >> cn;
+	switch (cn)
+	{
+		case 1: 
+		break;
+		case 2: 
+		break;
+		case 3: 
+		break;
+		case 4: 
+		break;
+		case 5: 
+		break;
+		case 6: 
+		break;
+		default:
+			
+	}
+}
+while (cn != 0);
+}#Code-ch-c-n-ng
